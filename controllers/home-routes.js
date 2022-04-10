1476adcc61655e376e9cc872df8dd2a63207640b
ethan fregien <ethanfregien@gmail.com>
2022-04-10 16:13:13 -0500

    email form started

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -5,4 +5,9 @@ router.get('/', (req, res) => {
     res.render('index')
 });
 
+router.get('/', (req, res) => {
+    res.sendFile(_dirname + '/views/exposure.ejs')
+    res.render('exposure')
+});
+
 module.exports = router;
