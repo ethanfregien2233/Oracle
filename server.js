2bd37e95223e5647ac84d534ed75343561af8fe6
ethan fregien <ethanfregien@gmail.com>
2022-04-10 14:52:29 -0500

    localhost error

--- server.js
+++ server.js
@@ -1,4 +1,10 @@
-const express = require('express')
+const express = require("express")
 const app = express()
+
+app.get('./', (req, res)=> {
+    console.log("Here")
+    res.sendStatus(500)
+    res.send("Hi")
+})
 
 app.listen(3000)
