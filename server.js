e2f6a4d69dd18ecda1c6fd7487931d6e5e600fe2
unknown <alexm1397@gmail.com>
2022-04-10 15:10:02 -0500

    updated server.js

--- server.js
+++ server.js
@@ -1,7 +1,23 @@
 const express = require("express")
-const app = express()
+//const routes
+//const sequelize
 
+//set app and port
+const app = express()
+const PORT = process.env.PORT || 3000;
+
+//set rendering eng
 app.set('view engine', 'ejs')
+
+//middleware here
+//set public to static
+app.use(express.static(__dirname + '/public'));
+//allows express to handle json data
+app.use(express.json());
+
+//routes
+// app.use(routes);
+
 
 app.get('./', (req, res)=> {
     console.log("Here")
@@ -9,4 +25,4 @@ app.get('./', (req, res)=> {
     res.send("Hi")
 })
 
-app.listen(3000)
+app.listen(PORT, ()=> console.log(`Now listening on ${PORT}`));
